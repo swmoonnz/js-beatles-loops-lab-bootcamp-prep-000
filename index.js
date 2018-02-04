efab715bb7a5d16a2b77c3d88cb12aa104714106
Sang Woo Moon <swmoonnz@gmail.com>
2018-02-04 23:41:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
-  for( var i = 0, i < musicians.length; i++)
-    {var string = ${musicians} plays ${instruments};
+  for(var i = 0, i < musicians.length; i++)
+    {var string =`${musicians} plays ${instruments}`;
     array.push(string);
   }
   return array
